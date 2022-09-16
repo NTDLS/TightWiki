b9377740a21967b8b4a7a89a14fa089645c6d71e
Josh Patterson <josh@ntdls.com>
2022-09-16 11:29:41 -0400

    vestige removal

--- AsapWikiCom/App_Start/BundleConfig.cs
+++ AsapWikiCom/App_Start/BundleConfig.cs
@@ -21,7 +21,7 @@ namespace AsapWikiCom
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/custom.css"));
         }
     }
 }
