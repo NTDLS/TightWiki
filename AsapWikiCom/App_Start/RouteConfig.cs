1c659f1a43b18232ecff7861726e2bb51e2d1cfe
Josh Patterson <josh@ntdls.com>
2022-09-26 12:44:17 -0400

    :|

--- AsapWikiCom/App_Start/RouteConfig.cs
+++ AsapWikiCom/App_Start/RouteConfig.cs
@@ -18,20 +18,20 @@ namespace AsapWikiCom
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Wiki", action = "v", id = UrlParameter.Optional }
+                defaults: new { controller = "Wiki", action = "Show", id = UrlParameter.Optional }
             );
             */
 
             routes.MapRoute(
                 name: "CategoryPage",
                 url: "{controller}/{action}/{navigation}",
-                defaults: new { controller = "Wiki", action = "v", navigation = "Home" }
+                defaults: new { controller = "Wiki", action = "Show", navigation = "Home" }
             );
             
             routes.MapRoute(
                 name: "DefaultWiki",
                 url: "{navigation}",
-                defaults: new { navigation = "Home", controller = "Wiki", action = "v" }
+                defaults: new { navigation = "Home", controller = "Wiki", action = "Show" }
             );
 
             routes.MapRoute(
