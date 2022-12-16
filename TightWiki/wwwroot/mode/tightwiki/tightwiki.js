79f479adb8b01d026900163bc4e6017ab18eb032
Josh Patterson <josh@ntdls.com>
2022-12-16 14:44:20 -0500

    Added namespace code hilighters.

--- TightWiki/wwwroot/mode/tightwiki/tightwiki.js
+++ TightWiki/wwwroot/mode/tightwiki/tightwiki.js
@@ -27,7 +27,7 @@
     "use strict";
 
     CodeMirror.defineMode("tightwiki", function () {
-        var functions = ["##set", "##get", "##color", "##tag", "##searchlist", "##taglist", "##searchcloud", "##tagglossary", "##recentlymodified", "##textglossary", "##tagcloud", "##image", "##file", "##related", "##tags", "##editlink", "##inject", "##include", "##br", "##hr", "##history", "##attachments", "##toc", "##title", "##navigation", "##name", "##created", "##lastmodified", "##appversion"],
+        var functions = ["##set", "##get", "##color", "##tag", "##searchlist", "##taglist", "##searchcloud", "##tagglossary", "##recentlymodified", "##textglossary", "##tagcloud", "##image", "##file", "##related", "##tags", "##editlink", "##inject", "##include", "##br", "##hr", "##history", "##attachments", "##toc", "##title", "##navigation", "##name", "##created", "##lastmodified", "##appversion", "##namespaceglossary", "##namespacelist","##namespace"],
             instructions = ["@@protect", "@@draft", "@@review", "@@deprecate", "@@include", "@@template"],
             scopes = ["bullets", "alert", "background", "collapse", "callout", "code", "foreground", "jumbotron", "card"];
 
