3fd6b81ee16f8061da94e57b5c1087cfce484e12
Josh Patterson <josh@ntdls.com>
2023-05-17 11:55:39 -0400

    Safer, saner database update.

--- CreateDatabaseSeeds/Program.cs
+++ CreateDatabaseSeeds/Program.cs
@@ -13,7 +13,7 @@ namespace CreateDatabaseSeeds
         {
             if (args.Length < 2)
             {
-                Console.WriteLine("Usage: CreateDatabaseSeeds.exe <path_with_scripts>");
+                Console.WriteLine("Usage: CreateDatabaseSeeds.exe <path_with_scripts> <output_path>");
             }
 
             string scriptPath = args[0];
@@ -25,19 +25,40 @@ namespace CreateDatabaseSeeds
             using var handler = new SqlConnectionHandler();
             var scriptFiles = Directory.GetFiles(scriptPath, "*.sql").ToList();
 
+            int maxFileSize = 1024 * 1024 * 5;
+
             foreach (var scriptFile in scriptFiles)
             {
                 Console.WriteLine("Writing: " + Path.GetFileNameWithoutExtension(scriptFile));
 
+                int currentFileSize = 0;
+                int currentFileCount = 1;
+
                 string scriptText = File.ReadAllText(scriptFile);
                 using SqlCommand command = new(scriptText, handler.Connection);
                 using SqlDataReader reader = command.ExecuteReader();
-                using StreamWriter sw = File.CreateText(Path.Combine(outputPath, Path.GetFileName(scriptFile)));
+                string newOutputFile = Path.Combine(outputPath, $"{Path.GetFileNameWithoutExtension(scriptFile)}_{currentFileCount:D4}{Path.GetExtension(scriptFile)}");
+                StreamWriter sw = File.CreateText(newOutputFile);
                 while (reader.Read())
                 {
-                    sw.WriteLine(reader.GetString(0));
+                    var currentLine = reader.GetString(0);
+                    sw.WriteLine(currentLine);
+
+                    if (currentFileSize >= maxFileSize && currentLine.ToLower() == "go")
+                    {
+                        currentFileSize = 0;
+                        currentFileCount++;
+                        sw.Close();
+                        sw.Dispose();
+
+                        newOutputFile = Path.Combine(outputPath, $"{Path.GetFileNameWithoutExtension(scriptFile)}_{currentFileCount:D5}{Path.GetExtension(scriptFile)}");
+                        sw = File.CreateText(newOutputFile);
+                    }
+
+                    currentFileSize += currentLine.Length;
                 }
                 sw.Close();
+                sw.Dispose();
             }
         }
     }
