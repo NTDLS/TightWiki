d9c7fabf67f887191f7526fd8229a0c1973858b9
Josh Patterson <josh@ntdls.com>
2024-05-21 16:32:43 -0400

    Fixed revisions syntax highlighting.

--- TightWiki/wwwroot/codemirror/mode/tightwiki/tightwiki.js
+++ TightWiki/wwwroot/codemirror/mode/tightwiki/tightwiki.js
@@ -27,7 +27,7 @@
     "use strict";
 
     CodeMirror.defineMode("tightwiki", function () {
-        var functions = ["##set", "##get", "##color", "##tag", "##searchlist", "##taglist", "##searchcloud", "##tagglossary", "##recentlymodified", "##textglossary", "##tagcloud", "##image", "##file", "##related", "##tags", "##editlink", "##inject", "##include", "##br", "##hr", "##history", "##attachments", "##toc", "##title", "##navigation", "##name", "##created", "##lastmodified", "##appversion", "##namespaceglossary", "##namespacelist","##namespace", "##snippet"],
+        var functions = ["##set", "##get", "##color", "##tag", "##searchlist", "##taglist", "##searchcloud", "##tagglossary", "##recentlymodified", "##textglossary", "##tagcloud", "##image", "##file", "##related", "##tags", "##editlink", "##inject", "##include", "##br", "##hr", "##revisions", "##attachments", "##toc", "##title", "##navigation", "##name", "##created", "##lastmodified", "##appversion", "##namespaceglossary", "##namespacelist","##namespace", "##snippet"],
             instructions = ["@@protect", "@@hidefootercomments", "@@nocache", "@@draft", "@@review", "@@deprecate", "@@include", "@@template"],
             scopes = ["bullets", "alert", "background", "collapse", "callout", "code", "foreground", "jumbotron", "card", "table", "stripedtable", "definesnippet", "order"];
 
