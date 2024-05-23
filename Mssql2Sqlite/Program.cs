6e42f1b836207e91bddc2d5bca3415f7a7e1ba51
Josh Patterson <josh@ntdls.com>
2024-05-23 14:46:22 -0400

    Updated MSSQL to SQLite migration tool.

---
+++ Mssql2Sqlite/Program.cs
@@ -0,0 +1,111 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+
+namespace Mssql2Sqlite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3 && args.Length != 5)
+            {
+                Console.WriteLine("Usage for SQL Server integrated security:");
+                Console.WriteLine("Mssql2Sqlite.exe <sqliteFile> <userGuid> <sqlServer>");
+
+                Console.WriteLine("");
+                Console.WriteLine("Usage for SQL Server user security:");
+                Console.WriteLine("Mssql2Sqlite.exe <sqliteFile> <userGuid> <sqlServer> <SQLServerUser> <SQLServerPassword>");
+
+                Console.WriteLine("sqliteFile: The SQLite file to import the data into, existing data will be deleted.");
+                Console.WriteLine("userGuid: The UserId from the SQLite users.db that you want to associate with the import.");
+                Console.WriteLine("sqlServer: The SQL Server name to export the data from.");
+                Console.WriteLine("SQLServerUser: Optional, is the username to use for connecting to SQL Server.");
+                Console.WriteLine("SQLServerPassword: Optional, is the password to use for connecting to SQL Server.");
+            }
+
+            string sqliteFile = args[0];
+            string userId = args[1];
+
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = args[2],
+                Encrypt = SqlConnectionEncryptOption.Optional,
+                TrustServerCertificate = true
+            };
+
+            if (args.Length == 3)
+            {
+                builder.IntegratedSecurity = true;
+            }
+            else if (args.Length == 5)
+            {
+                builder.IntegratedSecurity = false;
+                builder.UserID = args[3];
+                builder.Password = args[4];
+            }
+
+            MigrateTable("Page", sqliteFile, builder.ToString(), userId);
+            MigrateTable("PageFile", sqliteFile, builder.ToString(), userId);
+            MigrateTable("PageRevision", sqliteFile, builder.ToString(), userId);
+            MigrateTable("PageFileRevision", sqliteFile, builder.ToString(), userId);
+            MigrateTable("PageRevisionAttachment", sqliteFile, builder.ToString(), userId);
+        }
+
+        static void MigrateTable(string tableName,  string sqliteFile, string sqlServerConnectionString, string userId)
+        {
+            using var sqlServerConnection = new SqlConnection(sqlServerConnectionString);
+            var data = sqlServerConnection.Query<dynamic>($"SELECT * FROM [{tableName}]").ToList();
+            var insertColumns = sqlServerConnection.Query<string>($"SELECT name from sys.columns where object_id = object_id('{tableName}')").ToList();
+
+            using (var sqliteConnection = new SqliteConnection($"Data Source={sqliteFile}"))
+            {
+                sqliteConnection.Open();
+
+                // Enable writing of identity columns in SQLite
+                sqliteConnection.Execute("PRAGMA foreign_keys=OFF;");
+                sqliteConnection.Execute("BEGIN TRANSACTION;");
+                sqliteConnection.Execute("PRAGMA defer_foreign_keys=ON;");
+
+                var selectColumns = new List<string>();
+
+                foreach (var column in insertColumns)
+                {
+                    switch (column)
+                    {
+                        case "Namespace":
+                            selectColumns.Add("Coalesce(@Namespace, '')");
+                            break;
+                        case "CreatedByUserId":
+                        case "ModifiedByUserId":
+                            selectColumns.Add($"'{userId}'");
+                            break;
+                        default:
+                            selectColumns.Add($"@{column}");
+                            break;
+                    }
+                }
+
+                if (tableName == "PageFileRevision")
+                {
+                    insertColumns.Add("CreatedByUserId");
+                    selectColumns.Add("(SELECT P.ModifiedByUserId FROM PageFile as PF INNER JOIN Page as P ON P.Id = PF.PageId WHERE PF.Id = @PageFileId)");
+                }
+
+                sqliteConnection.Execute($"DELETE FROM {tableName}");
+
+                foreach (var item in data)
+                {
+                    var insertQuery = $"INSERT INTO {tableName} ({string.Join(",", insertColumns)}) SELECT {string.Join(",", selectColumns)}";
+                    sqliteConnection.Execute(insertQuery, (object?)item);
+                }
+
+                sqliteConnection.Execute("COMMIT;");
+                sqliteConnection.Execute("PRAGMA defer_foreign_keys=OFF;");
+                sqliteConnection.Execute("PRAGMA foreign_keys=ON;");
+            }
+
+            Console.WriteLine("Data import complete.");
+        }
+    }
+}
