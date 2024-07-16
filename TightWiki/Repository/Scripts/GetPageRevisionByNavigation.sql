c52cbc50da641a2cbdad7dc755e0530aa2df42f6
Josh Patterson <josh@ntdls.com>
2024-07-16 06:51:02 -0400

    Added revert link to page revision viewer.

--- TightWiki/Repository/Scripts/GetPageRevisionByNavigation.sql
+++ TightWiki/Repository/Scripts/GetPageRevisionByNavigation.sql
@@ -19,7 +19,6 @@ INNER JOIN [PageRevision] as PR
 	ON PR.PageId = P.Id
 INNER JOIN users_db.Profile as MBU
 	ON MBU.UserId = P.ModifiedByUserId
-
 WHERE
 	P.Navigation = @Navigation
 	AND PR.Revision = COALESCE(@Revision, P.Revision)
