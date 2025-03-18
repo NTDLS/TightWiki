c06b6a0571e98a1ae69509378f313607b0dfdbdf
Josh Patterson <Josh@NTDLS.com>
2025-03-17 22:08:53 -0400

    database update

---
+++ TightWiki.Repository/Scripts/Initialization/Versions/2.22.0/^000^Users^AddAnonymousRole.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO Role
+(
+	Name,
+	Description
+)
+SELECT
+	'Anonymous',
+	'Security group assigmed to users that are not logged in.'
+WHERE
+	NOT EXISTS(SELECT * FROM Role WHERE Name = 'Anonymous')
