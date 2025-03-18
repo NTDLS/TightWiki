0310ddda1e0e473d314e217e81b47cc649dddc0b
Josh Patterson <Josh@NTDLS.com>
2025-03-17 22:09:05 -0400

    typo

--- TightWiki.Repository/Scripts/Initialization/Versions/2.22.0/^000^Users^AddAnonymousRole.sql
+++ TightWiki.Repository/Scripts/Initialization/Versions/2.22.0/^000^Users^AddAnonymousRole.sql
@@ -5,6 +5,6 @@
 )
 SELECT
 	'Anonymous',
-	'Security group assigmed to users that are not logged in.'
+	'Security group assigned to users that are not logged in.'
 WHERE
 	NOT EXISTS(SELECT * FROM Role WHERE Name = 'Anonymous')
