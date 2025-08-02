447469cf247a562900142d19dd3d4f2601faf71b
Josh Patterson <Josh@NTDLS.com>
2025-08-02 12:42:17 -0400

    Resolved build warnings.

--- TightWiki.Library/DecimalModelBinder.cs
+++ TightWiki.Library/DecimalModelBinder.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.Extensions.Logging;
+using NTDLS.Helpers;
 using System.Globalization;
 
 namespace TightWiki.Library
 {
     public class InvariantDecimalModelBinderProvider : IModelBinderProvider
     {
-        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
         {
             if (context == null)
                 throw new ArgumentNullException(nameof(context));
@@ -15,7 +16,7 @@ namespace TightWiki.Library
             if (!context.Metadata.IsComplexType && (context.Metadata.ModelType == typeof(decimal) || context.Metadata.ModelType == typeof(decimal?)))
             {
                 var loggerFactory = context.Services.GetService(typeof(ILoggerFactory)) as ILoggerFactory;
-                return new InvariantDecimalModelBinder(context.Metadata.ModelType, loggerFactory);
+                return new InvariantDecimalModelBinder(context.Metadata.ModelType, loggerFactory.EnsureNotNull());
             }
 
             return null;
@@ -26,12 +27,10 @@ namespace TightWiki.Library
     {
         readonly SimpleTypeModelBinder _baseBinder;
 
-
         public InvariantDecimalModelBinder(Type modelType, ILoggerFactory loggerFactory)
         {
             _baseBinder = new SimpleTypeModelBinder(modelType, loggerFactory);
         }
-
 
         public Task BindModelAsync(ModelBindingContext context)
         {
@@ -40,14 +39,13 @@ namespace TightWiki.Library
 
             var valueProviderResult = context.ValueProvider.GetValue(context.ModelName);
 
-            if (valueProviderResult != ValueProviderResult.None)
+            if ( valueProviderResult != ValueProviderResult.None)
             {
                 context.ModelState.SetModelValue(context.ModelName, valueProviderResult);
 
-                var valueAsString = valueProviderResult.FirstValue.Replace(",", ".");
-                decimal result;
+                var valueAsString = valueProviderResult.FirstValue.EnsureNotNull().Replace(",", ".");
 
-                if (decimal.TryParse(valueAsString, NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out result))
+                if (decimal.TryParse(valueAsString, NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out decimal result))
                 {
                     context.Result = ModelBindingResult.Success(result);
                     return Task.CompletedTask;
