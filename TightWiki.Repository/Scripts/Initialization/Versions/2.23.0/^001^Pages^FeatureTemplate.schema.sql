d8167266e627dcd252cb9863c0f9a21296d5bd50
Josh Patterson <Josh@NTDLS.com>
2025-08-02 12:31:33 -0400

    Feature tamplates.

---
+++ TightWiki.Repository/Scripts/Initialization/Versions/2.23.0/^001^Pages^FeatureTemplate.schema.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE "FeatureTemplate" (
+	"Name"	TEXT NOT NULL UNIQUE COLLATE NOCASE,
+	"HelpPageId"	INTEGER,
+	"Description"	TEXT,
+	"TemplateText"	TEXT,
+	PRIMARY KEY("Name"),
+	FOREIGN KEY("HelpPageId") REFERENCES "Page"("Id")
+);
