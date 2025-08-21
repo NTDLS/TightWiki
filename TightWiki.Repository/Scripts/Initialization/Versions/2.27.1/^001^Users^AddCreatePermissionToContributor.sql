2040d94302b0cd7ef2f39b95f6ef16f5ccf42d79
Josh Patterson <Josh@NTDLS.com>
2025-08-21 12:38:55 -0400

    Added missing moderator/contributor role permissions. Adde emoji link.

---
+++ TightWiki.Repository/Scripts/Initialization/Versions/2.27.1/^001^Users^AddCreatePermissionToContributor.sql
@@ -0,0 +1,25 @@
+﻿--Add CREATE on ALL namespaces permissions for Contributor.
+INSERT INTO RolePermission(RoleId, PermissionId, Namespace, PageId, PermissionDispositionId)
+SELECT
+	(SELECT R.Id FROM Role as R WHERE R.Name = 'Contributor') as RoleId,
+	P.Id as PermissionId,
+	'*' as Namespace,
+	null as PageId,
+	(SELECT PD.Id FROM PermissionDisposition as PD WHERE PD.Name = 'Allow') as PermissionDispositionId
+FROM
+	Permission as P
+WHERE
+	P.Name IN ('Create')
+	AND NOT EXISTS (
+		SELECT
+			1
+		FROM
+			RolePermission as RP
+		INNER JOIN Role as R
+			ON R.Id = RP.RoleId
+		INNER JOIN Permission as P
+			ON P.Id = RP.PermissionId
+		WHERE
+			R.Name = 'Contributor'
+			AND P.Name = 'Create'
+	);
