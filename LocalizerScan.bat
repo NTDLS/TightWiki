73bc48f1fe424c382f936803abe499c40f7ede64
Josh Patterson <josh@ntdls.com>
2026-03-23 16:54:44 -0400

    New defaults and pause after translate.

--- LocalizerScan.bat
+++ LocalizerScan.bat
@@ -6,3 +6,4 @@ md Publish
 dotnet publish .\LocalizerScan -c Release -o publish\LocalizerScan --runtime win-x64 --self-contained false
 publish\LocalizerScan\LocalizerScan.exe ".\\" ".\TightWiki\Translations"
 
+pause
