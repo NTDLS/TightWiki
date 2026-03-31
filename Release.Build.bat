865ec629faa963e01f19aa0322cd1ee2556db9fa
Josh Patterson <josh@ntdls.com>
2026-03-31 16:35:08 -0400

    Build scripts.

--- Release.Build.bat
+++ Release.Build.bat
@@ -10,28 +10,29 @@ publish\GenerateSeedData\GenerateSeedData.exe ".\Data" ".\TightWiki.Repository\D
 REM -------------------Build
 rd .\Publish /q /s
 md Publish
-md Publish\TightWiki.Windows.x64
-md Publish\TightWiki.Windows.x64\data
-md Publish\TightWiki.Linux.x64
-md Publish\TightWiki.Linux.x64\data
+md Publish\TightWiki.Windows.x64\Deployment\data
+md Publish\TightWiki.Linux.x64\Deployment\data
 
-dotnet publish .\TightWiki -c Release -o publish\TightWiki.Windows.x64\Site --runtime win-x64 --self-contained false
+dotnet publish .\TightWiki -c Release -o publish\TightWiki.Windows.x64\Deployment\Site --runtime win-x64 --self-contained false
 dotnet publish .\TightWiki.Plugin.Default -c Release -o publish\TightWiki.Windows.x64\Plugins --runtime win-x64 --self-contained false
-md .\Publish\TightWiki.Windows.x64\Site\Plugins\
-Copy ".\Publish\TightWiki.Windows.x64\Plugins\TightWiki.Plugin.Default.dll" ".\Publish\TightWiki.Windows.x64\Site\Plugins\"
+md .\Publish\TightWiki.Windows.x64\Deployment\Site\Plugins\
+Copy ".\Publish\TightWiki.Windows.x64\Plugins\TightWiki.Plugin.Default.dll" ".\Publish\TightWiki.Windows.x64\Deployment\Site\Plugins\"
 
-dotnet publish .\TightWiki -c Release -o publish\TightWiki.Linux.x64\Site --runtime linux-x64 --self-contained false
+dotnet publish .\TightWiki -c Release -o publish\TightWiki.Linux.x64\Deployment\Site --runtime linux-x64 --self-contained false
 dotnet publish .\TightWiki.Plugin.Default -c Release -o publish\TightWiki.Linux.x64\Plugins --runtime win-x64 --self-contained false
-md .\Publish\TightWiki.Linux.x64\Site\Plugins\
-Copy ".\Publish\TightWiki.Linux.x64\Plugins\TightWiki.Plugin.Default.dll" ".\Publish\TightWiki.Linux.x64\Site\Plugins\"
+md .\Publish\TightWiki.Linux.x64\Deployment\Site\Plugins\
+Copy ".\Publish\TightWiki.Linux.x64\Plugins\TightWiki.Plugin.Default.dll" ".\Publish\TightWiki.Linux.x64\Deployment\Site\Plugins\"
 
-REM -------------------Package
+REM -------------------Make sure the defaults.db is not packaged.
 del .\Data\defaults.db
-copy .\Data\*.* Publish\TightWiki.Windows.x64\data
-copy .\Data\*.* Publish\TightWiki.Linux.x64\data
 
-7z.exe a -tzip -r -mx9 ".\Publish\TightWiki.Windows.x64.zip" ".\Publish\TightWiki.Windows.x64\*.*"
-7z.exe a -tzip -r -mx9 ".\Publish\TightWiki.Linux.x64.zip" ".\Publish\TightWiki.Linux.x64\*.*"
+copy .\Data\*.* Publish\TightWiki.Windows.x64\Deployment\data
+copy .\Data\*.* Publish\TightWiki.Linux.x64\Deployment\data
+
+7z.exe a -tzip -r -mx9 ".\Publish\TightWiki.Windows.x64.zip" ".\Publish\TightWiki.Windows.x64\Deployment\*.*"
+7z.exe a -tzip -r -mx9 ".\Publish\TightWiki.Linux.x64.zip" ".\Publish\TightWiki.Linux.x64\Deployment\*.*"
+
+dotnet pack .\TightWiki.Plugin -c Release -o ".\Publish"
 
 rd .\Publish\TightWiki.Windows.x64 /q /s
 rd .\Publish\TightWiki.Linux.x64 /q /s
