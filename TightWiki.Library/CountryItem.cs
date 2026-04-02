2b1577a86864bc33cb78d132e80fdd4d10154a55
Josh Patterson <josh@ntdls.com>
2026-04-02 14:33:49 -0400

    Fixed bogus country and optimized enumeration.

--- TightWiki.Library/CountryItem.cs
+++ TightWiki.Library/CountryItem.cs
@@ -10,11 +10,15 @@ namespace TightWiki.Library
         public static List<CountryItem> GetAll()
         {
             var list = new List<CountryItem>();
+            var check = new HashSet<string>();
 
             foreach (var ci in CultureInfo.GetCultures(CultureTypes.SpecificCultures))
             {
                 var regionInfo = new RegionInfo(ci.Name);
-                if (list.Where(o => o.Value == regionInfo.Name).Any() == false)
+                if (regionInfo.Name == "001" || regionInfo.EnglishName.Equals("World", StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                if (check.Add(regionInfo.Name))
                 {
                     list.Add(new CountryItem
                     {
