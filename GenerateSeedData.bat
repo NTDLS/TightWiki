5627243ff328ace6ae3da98eecb79cfcce912c3a
Josh Patterson <josh@ntdls.com>
2026-04-03 12:40:55 -0400

    Translations.

--- GenerateSeedData.bat
+++ GenerateSeedData.bat
@@ -4,3 +4,5 @@ rd .\Publish /q /s
 md Publish
 dotnet publish .\GenerateSeedData -c Release -o publish\GenerateSeedData --runtime win-x64 --self-contained false
 publish\GenerateSeedData\GenerateSeedData.exe ".\Data" ".\TightWiki.Repository\Defaults"
+
+pause
