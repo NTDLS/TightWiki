1a3a4326c08e0f008ac7b46ee511368f3f35e91c
Josh Patterson <josh@ntdls.com>
2026-04-10 10:14:51 -0400

    Hotfix roll forward from 2.34.x

--- TightWiki.Repository/Scripts/Initialization/Versions/2.31.3/^001^Statistics^NewStatsStructure.sql
+++ TightWiki.Repository/Scripts/Initialization/Versions/2.31.3/^001^Statistics^NewStatsStructure.sql
@@ -1,3 +1,5 @@
-﻿ALTER TABLE CompilationStatistics RENAME TO PageStatistics;
+﻿--##IF TABLE NOT EXISTS(PageStatistics)
+
+ALTER TABLE CompilationStatistics RENAME TO PageStatistics;
 
 ALTER TABLE PageStatistics ADD "TotalViewCount" INTEGER NOT NULL DEFAULT 0;
